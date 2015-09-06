554c56010a354582433fbeb229be90dd0d0f2aaf
ziban <csirmah@gmail.com>
2015-09-05 23:10:41 -0400

    Added all the router definition

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//TODO: To be deleted
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+var bookController = require('/routes/books')
 
 var app = express();
+var router = express.Router();
+
+//TODO: Connection to a mongodb database
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +29,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//TODO: Might not need this at all. To be removed
+//app.use('/', routes);
+//app.use('/users', users);
+
+//Endpoint definition for all the books call
+router.route('/books')
+  .get(bookController.getBooks)
+  .post(bookController.postBooks)
+  .delete(bookController.deleteBooks)
+  .put(bookController.putBooks)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
