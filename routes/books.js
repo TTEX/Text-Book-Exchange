c67c42ac6f409c29ebd725755f1a39faa05e88b1
ziban <csirmah@gmail.com>
2015-09-05 22:58:24 -0400

    Added book definitions

---
+++ routes/books.js
@@ -0,0 +1,19 @@
+var books = require('../models/booksModels')
+
+
+exports.postBooks = function(req, res) {
+
+
+}
+
+exports.getBooks = function(req, res) {
+
+}
+
+exports.putBooks = function(req, res) {
+
+}
+
+exports.deleteBooks = function(req, res) {
+
+}
